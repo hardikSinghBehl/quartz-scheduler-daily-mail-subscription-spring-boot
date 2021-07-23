ce2413941815e3cc6e313e3e0607bf4e49c5ac0e
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-07-23 13:45:10 +0530

    initial project commit

---
+++ src/main/java/com/hardik/mercury/DailyMailQuartzSchedulerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.mercury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMailQuartzSchedulerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMailQuartzSchedulerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/mercury/DailyMailQuartzSchedulerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.mercury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMailQuartzSchedulerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
